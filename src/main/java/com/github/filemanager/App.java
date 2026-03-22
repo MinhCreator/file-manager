bb5b959b068e0cf6d21beff929fbd1e69cfe4095
MinhCreator <92376650+MinhCreator@users.noreply.github.com>
2026-03-22 10:37:56 +0700

    changing mainClass for auto-build

--- src/main/java/com/github/filemanager/App.java
+++ src/main/java/com/github/filemanager/App.java
@@ -1,5 +1,7 @@
 package com.github.filemanager;
 
+import com.github.filemanager.component.FileManager;
+
 public class App {
     public static void main(String[] args) {
         new FileManager();
--- src/main/java/com/github/filemanager/component/FileManager.java
+++ src/main/java/com/github/filemanager/component/FileManager.java
@@ -21,12 +21,10 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
-package com.github.filemanager;
+package com.github.filemanager.component;
 
 import com.formdev.flatlaf.FlatLightLaf;
 import com.formdev.flatlaf.extras.FlatSVGIcon;
-import com.github.filemanager.component.FileTableModel;
-import com.github.filemanager.component.FileTreeCellRenderer;
 import com.github.filemanager.operation.BackgroundFileOperations;
 import com.github.filemanager.service.FileCache;
 import com.github.filemanager.service.PerformanceMonitor;
